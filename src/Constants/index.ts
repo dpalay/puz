d50ec28c05f49e29a2d0b42488dd3993c960aa17
Dave Palay <dpalay@epic.com>
2020-04-26 20:06:34 -0500

    Golarian dieties

--- src/Constants/index.ts
+++ src/Constants/index.ts
@@ -1,6 +1,11 @@
 import alpha from './alpha'
 import states from './stateList'
 import countries from './countryList'
+import golarianDieties from './golarianDieties'
 
-const puzzles: {name: string, words: string[]}[] = [{name: "states", words: states}, {name: "countries", words: countries}]
+const puzzles: {name: string, words: string[]}[] = [
+    {name: "states", words: states}, 
+    {name: "countries", words: countries},
+    {name: "Pathfinder Deities", words: golarianDieties}
+]
 export {alpha, puzzles}
