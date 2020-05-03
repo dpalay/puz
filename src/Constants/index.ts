a7d1e3be67c3b8eed7448427e4513245b51c1cd5
Dave Palay <dpalay@epic.com>
2020-05-02 23:04:30 -0500

    adding common english words

--- src/Constants/index.ts
+++ src/Constants/index.ts
@@ -2,10 +2,13 @@ import alpha from './alpha'
 import states from './stateList'
 import countries from './countryList'
 import golarianDieties from './golarianDieties'
+import commonWords from './commonWords'
 
 const puzzles: {name: string, words: string[]}[] = [
+    {name: "Most common English Words", words: commonWords},
     {name: "states", words: states}, 
     {name: "countries", words: countries},
     {name: "Pathfinder Deities", words: golarianDieties}
+
 ]
 export {alpha, puzzles}
