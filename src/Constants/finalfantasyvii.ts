3b53ae1f3752f19652f2c33e3a3cc29a7568259b
Dave Palay <dpalay@epic.com>
2020-05-04 11:53:47 -0500

    adding ff7

---
+++ src/Constants/finalfantasyvii.ts
@@ -0,0 +1,18 @@
+export default [
+"AERITH",
+"BARRET",
+"CAIT SITH",
+"CID",
+"CLOUD",
+"HIGHWIND",
+"RED XIII",
+"RENO",
+"RUDE",
+"RUFUS",
+"SEPHIROTH",
+"SHINRA",
+"TIFA",
+"TSENG",
+"VINCENT",
+"YUFFIE",
+]
--- src/Constants/index.ts
+++ src/Constants/index.ts
@@ -3,12 +3,14 @@ import states from './stateList'
 import countries from './countryList'
 import golarianDieties from './golarianDieties'
 import commonWords from './commonWords'
+import finalfantasy7 from './finalfantasyvii'
 
 const puzzles: {name: string, words: string[]}[] = [
+    {name: "States", words: states}, 
+    {name: "Countries", words: countries},
+    {name: "Pathfinder Deities", words: golarianDieties},
+    {name: "Final Fantasy 7", words: finalfantasy7},
     {name: "Most common English Words", words: commonWords},
-    {name: "states", words: states}, 
-    {name: "countries", words: countries},
-    {name: "Pathfinder Deities", words: golarianDieties}
 
 ]
 export {alpha, puzzles}
