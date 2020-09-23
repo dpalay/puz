7bda317809346a8b6b180e3dd1dc3469d34665ce
Dave Palay <dpalay@epic.com>
2020-09-23 08:58:04 -0500

    update client classes

--- src/Classes/cell.ts
+++ src/Classes/cell.ts
@@ -1,26 +1,23 @@
-import {Direction} from '../Structures/'
 import {v4 as uuidv4} from 'uuid'
-import { Word, Puzzle } from '.';
 
 class Cell {
     id: string;
     row: number;
     col: number;
     value: string;
-    words: Word[]; //ID of the words this cell contains
+    words: string[]; //ID of the words this cell contains
     garbage: boolean; // is this just filler?
   
-    constructor(value: string, [row, col]: [number, number]) {
-        this.id = uuidv4()
+    constructor(value: string, [row, col]: [number, number], wordIDs?: string[]){
+      this.id = uuidv4()
       this.row = row;
       this.col = col;
       this.value = value;
       this.words = [];
-      this.garbage = true;
+      this.garbage = value===" ";
+      this.words = wordIDs ? [...wordIDs] : []
     }
-    move = (direction: Direction): [number, number] => {
-      return [this.row + Puzzle.dy[direction], this.col + Puzzle.dx[direction]];
-    };
+    
     pos = (): [number, number] => [this.row, this.col];
   }
 export default Cell
