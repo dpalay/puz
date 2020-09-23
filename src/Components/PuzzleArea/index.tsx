6674bfa0ba2d059ab2c35788620b8f5db54db2ef
Dave Palay <dpalay@epic.com>
2020-09-23 08:59:30 -0500

    integrate axios and server-side puzzle build

--- src/Components/PuzzleArea/index.tsx
+++ src/Components/PuzzleArea/index.tsx
@@ -3,5 +3,6 @@ import WordList from './WordList/WordList';
 import WordEntry from './WordEntry/WordEntry';
 import PuzCell from './PuzCell/PuzCell';
 import ClueList from './ClueList/ClueList';
+import PuzContainer from './PuzContainer/PuzContainer'
 
-export  {PuzzleArea, WordEntry, WordList, PuzCell, ClueList}
+export  {PuzzleArea, WordEntry, WordList, PuzCell, ClueList, PuzContainer}
