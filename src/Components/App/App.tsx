c23f49a6076da80f64cce73981f2c193e1d6b84b
Dave Palay <dpalay@epic.com>
2020-09-24 16:55:27 -0500

    make the swap

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,21 +1,26 @@
-import React, { useState } from 'react';
-import './App.css';
-import TopMenu from '../TopMenu';
-import {PuzzleArea} from '../PuzzleArea';
-import { Word } from '../../Classes';
+import React, { useState } from "react";
+import "./App.css";
+import TopMenu from "../TopMenu";
+import { PuzzleArea } from "../PuzzleArea";
+import { Word } from "../../Classes";
+import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
+import { wordList } from "../../Recoil";
 
 const App: React.FC = () => {
+  //const [words, setWords] = useState<Word[]>([]);
+  const [words, setWords] = useRecoilState(wordList)
 
-  const [words, setWords] = useState<Word[]>([])
 
   return (
-    <div className="App">
-      <TopMenu setWords={setWords} hasWords={words.length > 0} />
-      <main style={{background: "lightblue"}}>
-        <PuzzleArea words={words} setWords={setWords} />
-      </main>
-    </div>
+    <RecoilRoot>
+      <div className="App">
+        <TopMenu setWords={setWords} hasWords={words.length > 0} />
+        <main style={{ background: "lightblue" }}>
+          <PuzzleArea words={words} setWords={setWords} />
+        </main>
+      </div>
+    </RecoilRoot>
   );
-}
+};
 
 export default App;
