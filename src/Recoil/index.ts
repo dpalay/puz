ac13c7ee057fd8360ad20c370aeb7517d0c159d8
Dave Palay <dpalay@epic.com>
2020-09-25 10:43:05 -0500

    added selectedWord atom

--- src/Recoil/index.ts
+++ src/Recoil/index.ts
@@ -1,16 +1,20 @@
-import {atom, selector} from 'recoil'
-import { Word } from '../Classes'
-
+import { atom, selector } from "recoil";
+import { Word } from "../Classes";
 
 export const wordList = atom<Word[]>({
-    key: 'wordList',
-    default: []
-})
+  key: "wordList",
+  default: [],
+});
+
+export const selectedWord = atom<Word | undefined>({
+  key: "selectedWord",
+  default: undefined,
+});
 
 export const hasWords = selector<boolean>({
-    key: 'hasWords',
-    get: ({get}) => {
-        const hasWords = get(wordList)
-        return hasWords.length > 0
-    }
-})
+  key: "hasWords",
+  get: ({ get }) => {
+    const hasWords = get(wordList);
+    return hasWords.length > 0;
+  },
+});
