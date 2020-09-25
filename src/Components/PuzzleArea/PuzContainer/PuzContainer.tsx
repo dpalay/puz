09dbf764affa879398819f95221f2cff1967323f
Dave Palay <dpalay@epic.com>
2020-09-25 11:05:38 -0500

    Style

--- src/Components/PuzzleArea/PuzContainer/PuzContainer.tsx
+++ src/Components/PuzzleArea/PuzContainer/PuzContainer.tsx
@@ -2,59 +2,59 @@ import { Row } from "antd";
 import React, { useMemo } from "react";
 import { ClueList, PuzCell } from "..";
 import ReturnData from "../../../../functions/src/returnData";
-import {Puzzle, Word} from '../../../Classes'
-import {AxiosError} from 'axios'
+import { Puzzle, Word } from "../../../Classes";
+import { AxiosError } from "axios";
 
 interface IProps {
-    data: ReturnData    
-    words: Word[]
-    showFill: boolean
-    loading: boolean
-    error: AxiosError<any> | undefined
-    selectedWord?: Word
+  data: ReturnData;
+  words: Word[];
+  showFill: boolean;
+  loading: boolean;
+  error: AxiosError<any> | undefined;
+  selectedWord?: Word;
 }
 
-
 const PuzContainer: React.FC<IProps> = (props: IProps) => {
-    const {data, words, showFill, loading, error, selectedWord} = props
+  const { data, words, showFill, loading, selectedWord } = props;
 
-    let puzzle = useMemo(() => {
-        if (data && data.puzzle && data.lettersUsed && data.status === "success") {
-          return new Puzzle(data.puzzle, data.lettersUsed);
-        }
-      }, [data]);
+  let puzzle = useMemo(() => {
+    if (data && data.puzzle && data.lettersUsed && data.status === "success") {
+      return new Puzzle(data.puzzle, data.lettersUsed);
+    }
+  }, [data]);
 
-      if (loading) { return <p>Loading!</p>}
+  if (loading) {
+    return <p>Loading!</p>;
+  }
   return (
-  <>
-  <Row>
-  {words.length > 0 ? (
-    <table className="puzTable">
-      <tbody>
-        {puzzle?.board
-          .slice(1, puzzle.board.length - 1)
-          .map((row, i) => (
-            <tr key={`row${i}`}>
-              {row.slice(1, row.length - 1).map((cell) => (
-                <PuzCell
-                  key={cell.id}
-                  cell={cell}
-                  showFill={showFill}
-                  selectedWordId={selectedWord?.id}
-                />
+    <>
+      <Row>
+        {words.length > 0 ? (
+          <table className="puzTable">
+            <tbody>
+              {puzzle?.board.slice(1, puzzle.board.length - 1).map((row, i) => (
+                <tr key={`row${i}`}>
+                  {row.slice(1, row.length - 1).map((cell) => (
+                    <PuzCell
+                      key={cell.id}
+                      cell={cell}
+                      showFill={showFill}
+                      selectedWordId={selectedWord?.id}
+                    />
+                  ))}
+                </tr>
               ))}
-            </tr>
-          ))}
-      </tbody>
-    </table>
-  ) : (
-    "Add some words!"
-  )}
-</Row>
-<Row className="only-print">
-  <ClueList />
-</Row>
-</>)
+            </tbody>
+          </table>
+        ) : (
+          "Add some words!"
+        )}
+      </Row>
+      <Row className="only-print">
+        <ClueList />
+      </Row>
+    </>
+  );
 };
 
 export default PuzContainer;
--- src/Components/PuzzleArea/WordList/WordList.tsx
+++ src/Components/PuzzleArea/WordList/WordList.tsx
@@ -1,46 +1,58 @@
-import React from 'react'
-import { List, Tooltip } from 'antd'
-import { Word } from '../../../Classes';
-import { CloseCircleOutlined } from '@ant-design/icons';
-import {wordList, selectedWord as selectedWordAtom} from '../../../Recoil'
-import {useRecoilState} from 'recoil'
-import './WordList.css'
+import React from "react";
+import { List, Tooltip } from "antd";
+import { Word } from "../../../Classes";
+import { CloseCircleOutlined } from "@ant-design/icons";
+import { wordList, selectedWord as selectedWordAtom } from "../../../Recoil";
+import { useRecoilState } from "recoil";
+import "./WordList.css";
 
-
-
-interface Iprops {
-}
-
-
+interface Iprops {}
 
 const WordList: React.FC<Iprops> = (props: Iprops) => {
-    const [words, setWords] = useRecoilState(wordList)
-    const [selectedWord, setSelectedWord] = useRecoilState(selectedWordAtom)
-    const computeStyle = (word: Word) => {
-        if (word === selectedWord) {
-            return { background: "green" }
-        }
+  const [words, setWords] = useRecoilState(wordList);
+  const [selectedWord, setSelectedWord] = useRecoilState(selectedWordAtom);
+  const computeStyle = (word: Word) => {
+    if (word === selectedWord) {
+      return { background: "green" };
     }
-    const removeWord = (wordToRemove: Word) => {
-        setWords(words.filter(word => word !== wordToRemove))
+  };
+  const removeWord = (wordToRemove: Word) => {
+    setWords(words.filter((word) => word !== wordToRemove));
+  };
+  const handleSelect = (wordToSelect: Word) => {
+    if (selectedWord === wordToSelect) {
+      setSelectedWord(undefined);
+    } else {
+      setSelectedWord(wordToSelect);
     }
-    const handleSelect = (wordToSelect: Word) => {
-        if (selectedWord === wordToSelect) { setSelectedWord(undefined) }
-        else { setSelectedWord(wordToSelect) }
-    }
+  };
 
-    return (
-        <List className="word_list" size="small" dataSource={words} renderItem={word => (
-            <List.Item key={word.id} onClick={() => handleSelect(word)} style={{ cursor: "default", ...computeStyle(word) }} onContextMenu={(e: React.SyntheticEvent) => { e.preventDefault(); removeWord(word) }}>
-                <Tooltip title="Remove">
-                    <CloseCircleOutlined onClick={() => removeWord(word) } style={{ color: "red" }} />
-                </Tooltip>
-                {word.word}
-            </List.Item>)} />
-
-
-    )
-
-}
+  return (
+    <List
+      className="word_list"
+      size="small"
+      dataSource={words}
+      renderItem={(word) => (
+        <List.Item
+          key={word.id}
+          onClick={() => handleSelect(word)}
+          style={{ cursor: "default", ...computeStyle(word) }}
+          onContextMenu={(e: React.SyntheticEvent) => {
+            e.preventDefault();
+            removeWord(word);
+          }}
+        >
+          <Tooltip title="Remove">
+            <CloseCircleOutlined
+              onClick={() => removeWord(word)}
+              style={{ color: "red" }}
+            />
+          </Tooltip>
+          {word.word}
+        </List.Item>
+      )}
+    />
+  );
+};
 
 export default WordList;
